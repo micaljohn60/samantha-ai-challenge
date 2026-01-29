6471487b4e4f64867509bd98068cb677c9dfa514
thei0438 <micaljohn60@gmail.com>
2026-01-30 00:56:45 +1100

    fix db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,12 @@
 import { Pool } from "pg";
 
-const pool =
-  global.pgPool ||
-  new Pool({
-    host: process.env.PG_HOST,
-    port: Number(process.env.PG_PORT || 5432),
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
-    ssl: false,
-  });
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT || 5432),
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  ssl: false,
+});
 
 export default pool;
